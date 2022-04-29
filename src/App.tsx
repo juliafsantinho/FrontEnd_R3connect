803fbfb6eacfb56241d898715dc777181168072e
Santos <santos.nicolle@aluno.ufabc.edu.br>
2022-04-29 11:17:47 -0300

    adicionando componentes estáticos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Footer from './componentes/estaticos/footer/Footer';
 
 function App() {
   return (
-   <h1>titulo</h1>
+    <>
+        <Navbar />
+
+        <Footer />
+    </>
   );
 }
 
---
+++ src/componentes/estaticos/footer/Footer.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import GitHubIcon from "@material-ui/icons/GitHub"
+import {Typography, Box, Grid } from '@material-ui/core';
+
+import './Footer.css';
+
+function Footer() {
+
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+                <Grid alignItems="center" item xs={12}>
+                    <Box className='box1'>
+                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                            <Typography variant="h6" align="center" className='textos'>Siga-nos nas redes sociais </Typography>
+                        </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <a href="https://www.github.com/santosnicolle" target="_blank">
+                            <GitHubIcon className='redes' />
+                            </a>
+                        </Box>
+                    </Box>
+                    <Box className='box2'>
+                        <Box paddingTop={1}>
+                            <Typography variant="subtitle2" align="center" className='textos' >© 2022 Copyright:</Typography>
+                        </Box>
+                        <Box>
+                            <a target="_blank" href="https://brasil.generation.org">
+                                <Typography variant="subtitle2" className='textos' align="center">r3connectecommerce.herokuapp.com</Typography>
+                            </a>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </>
+    );
+
+}
+
+export default Footer;
