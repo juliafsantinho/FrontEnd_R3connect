c973a0e4c678b758ef1fcf1eda4069b511c1a458
CristianoFelipe <cristianofnunes97@gmail.com>
2022-04-29 14:23:21 -0300

    criação classe login

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Footer from './componentes/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+
 
 function App() {
   return (
     <>
         <Navbar />
         <Home />
+        <Login />
         <Footer />
     </>
   );
