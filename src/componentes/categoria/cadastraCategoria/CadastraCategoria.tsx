515ee6329a66021ae96f7d176c95a35089b9f73a
Santos <santos.nicolle@aluno.ufabc.edu.br>
2022-05-11 11:19:12 -0300

    adicionando delete, post e put na cadastro de categoria

--- src/componentes/categoria/cadastraCategoria/CadastraCategoria.tsx
+++ src/componentes/categoria/cadastraCategoria/CadastraCategoria.tsx
@@ -1,13 +1,125 @@
-import React from "react";
+import { Button, Container, TextField, Typography } from "@material-ui/core";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import Categoria from "../../../models/Categoria";
+import { buscaId, post, put } from "../../../services/Service";
+import { TokenState } from "../../../store/tokens/tokensReducer";
 
 function CadastraCategoria(){
-    return(
-        <>
-        
-        </>
-    )
 
+    let navigate = useNavigate();
+    const { id } = useParams<{id: string}>();
+    
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+
+    const [categoria, setCategoria] = useState<Categoria>({
+        id: 0,
+        descricao: '',
+        material: ''
+    })
+
+    useEffect(() => {
+        if(token == "") {
+            toast.error('Você precisa estar logado', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+            navigate("/login")
+        }
+    }, [token])
+
+    useEffect(() =>{
+        if(id !== undefined){
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        buscaId(`/categoria/${id}`, setCategoria, {
+            headers: {
+              'Authorization': token
+            }
+          })
+    }
+
+    function updatedCategoria(e: ChangeEvent<HTMLInputElement>) {
+
+        setCategoria({
+            ...categoria,
+            [e.target.name]: e.target.value,
+        })
+
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+        console.log("tema " + JSON.stringify(categoria))
+
+        if (id !== undefined) {
+            console.log(categoria)
+            put(`/categoria`, categoria, setCategoria, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            toast.success('Categoria atualizada com sucesso', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+        } else {
+            post(`/categoria`, categoria, setCategoria, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            toast.success('Categoria cadastrada com sucesso', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+        }
+        back()
+
+    }
+
+    function back() {
+        navigate('/categoria')
+    }
+  
+    return (
+        <Container maxWidth="sm" className="topo">
+            <form onSubmit={onSubmit}>
+                <Typography variant="h3" color="textSecondary" component="h1" align="center" >Formulário de cadastro de categoria</Typography>
+                <TextField value={categoria.descricao} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedCategoria(e)} id="descricao" label="descricao" variant="outlined" name="descricao" margin="normal" fullWidth />
+                <Button type="submit" variant="contained" color="primary">
+                    Finalizar
+                </Button>
+            </form>
+        </Container>
+    )
 }
 
 
 export default CadastraCategoria;
+
