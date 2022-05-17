f584d3bfc8d48bd8d0560c81b75dd19f7b72910a
lucassodrealves <lucassodrealves4@gmail.com>
2022-05-17 12:49:46 -0300

    arrumando rota do cancelar do carrinho

--- src/componentes/cart/Cart.tsx
+++ src/componentes/cart/Cart.tsx
@@ -134,7 +134,7 @@ function Cart() {
                         </Box>
                     </Box>
 
-                    <Link to="/posts" className="cardProductButton">
+                    <Link to="/listarProduto" className="cardProductButton">
                         <Box mx={1}>
                             <Button variant="contained" size='small' color="secondary">
                                 Cancelar
